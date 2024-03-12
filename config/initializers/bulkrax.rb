8aff2e3b9da0c94657e26a97dc3f1c5c76c79f86
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2024-03-12 15:07:12 -0700

    override bulkrax's default split regex
    This ensures all field mappings not listed in our local copy respect the
    same rules. Bulkrax's default is not suitable for Mobius' incoming CSV
    data

--- config/initializers/bulkrax.rb
+++ config/initializers/bulkrax.rb
@@ -2,30 +2,33 @@
 
 # Ensure Knapsack version gets loaded after Hyku's bulkrax.rb
 Rails.application.config.after_initialize do
+  # OVERRIDE Bulkrax v5.4.1
+  # Override default split regex to only split on unescaped comma and pipe chars
+  Bulkrax.instance_variable_set(:@multi_value_element_split_on, /\s*(?<!\\)[,|]\s*/)
+
   Bulkrax.setup do |config|
     hyku_csv_field_mappings = config.field_mappings['Bulkrax::CsvParser']
 
     config.field_mappings['Bulkrax::CsvParser'] = hyku_csv_field_mappings.merge({
-      # FIXME: these split configs don't fully work as intended
-      'contributor' => { from: %w[sm_contributor contributor], split: /\s*(?<!\\)[,|]\s*/ },
-      'creator' => { from: %w[sm_creator creator], split: /\s*(?<!\\)[,|]\s*/ },
-      'date_created' => { from: %w[sm_date date_created], split: /\s*(?<!\\)[,|]\s*/ },
-      'description' => { from: %w[tm_description description], split: /\s*(?<!\\)[,|]\s*/ },
-      'identifier' => { from: %w[ss_pid identifier], split: /\s*(?<!\\)[,|]\s*/, source_identifier: true },
-      'language' => { from: %w[sm_language language], split: /\s*(?<!\\)[,|]\s*/ },
-      'parents' => { from: %w[sm_collection parents], split: /\s*(?<!\\)[,|]\s*/, related_parents_field_mapping: true },
-      'publisher' => { from: %w[sm_publisher publisher], split: /\s*(?<!\\)[,|]\s*/ },
-      'resource_type' => { from: %w[sm_type resource_type], split: /\s*(?<!\\)[,|]\s*/ },
-      'source' => { from: %w[sm_source source], split: /\s*(?<!\\)[,|]\s*/ },
-      'subject' => { from: %w[sm_subject subject], split: /\s*(?<!\\)[,|]\s*/ },
-      'title' => { from: %w[sm_title title], split: /\s*(?<!\\)[,|]\s*/ },
+      'contributor' => { from: %w[sm_contributor contributor], split: true },
+      'creator' => { from: %w[sm_creator creator], split: true },
+      'date_created' => { from: %w[sm_date date_created], split: true },
+      'description' => { from: %w[tm_description description], split: true },
+      'identifier' => { from: %w[ss_pid identifier], split: true, source_identifier: true },
+      'language' => { from: %w[sm_language language], split: true },
+      'parents' => { from: %w[sm_collection parents], split: true, related_parents_field_mapping: true },
+      'publisher' => { from: %w[sm_publisher publisher], split: true },
+      'resource_type' => { from: %w[sm_type resource_type], split: true },
+      'source' => { from: %w[sm_source source], split: true },
+      'subject' => { from: %w[sm_subject subject], split: true },
+      'title' => { from: %w[sm_title title], split: true },
       # Custom property mappings
-      'collection' => { from: %w[bs_iscollection collection], split: /\s*(?<!\\)[,|]\s*/ },
-      'community' => { from: %w[bs_iscommunity community], split: /\s*(?<!\\)[,|]\s*/ },
-      'coverage' => { from: %w[sm_coverage coverage], split: /\s*(?<!\\)[,|]\s*/ },
-      'file_format' => { from: %w[sm_format file_format], split: /\s*(?<!\\)[,|]\s*/ },
-      'relation' => { from: %w[sm_relation relation], split: /\s*(?<!\\)[,|]\s*/ },
-      'rights' => { from: %w[sm_rights rights], split: /\s*(?<!\\)[,|]\s*/ }
+      'collection' => { from: %w[bs_iscollection collection], split: true },
+      'community' => { from: %w[bs_iscommunity community], split: true },
+      'coverage' => { from: %w[sm_coverage coverage], split: true },
+      'file_format' => { from: %w[sm_format file_format], split: true },
+      'relation' => { from: %w[sm_relation relation], split: true },
+      'rights' => { from: %w[sm_rights rights], split: true }
     })
   end
 end
