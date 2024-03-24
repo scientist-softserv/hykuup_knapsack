88c7352a8ec9557edba71e77e0fe27ae52dde176
Kirk Wang <kirk.wang@scientist.com>
2024-03-23 20:53:52 -0700

    🧹 Add better spec coverage for CsvParserDecorator
    This commit will flesh out the CsvParserDecorator spec to cover more of
    how the combined csv should look.

--- spec/parsers/bulkrax/csv_parser_decorator_spec.rb
+++ spec/parsers/bulkrax/csv_parser_decorator_spec.rb
@@ -63,6 +63,51 @@ RSpec.describe Bulkrax::CsvParserDecorator, type: :decorator do
     end
   end
 
+  describe 'the Combined CSV' do
+    before do
+      allow(csv_parser).to receive(:clean_up_upacked_files)
+      allow(Account).to receive(:find_by).and_return(account)
+      csv_parser.unzip(file_to_unzip)
+    end
+
+    let(:combined_csv_path) { File.join(importer_unzip_path, Bulkrax::CsvParser::COMBINED_CSV_FILENAME) }
+    let(:combined_csv_data) { CSV.read(combined_csv_path, headers: true).map(&:to_h) }
+    let(:original_csv_paths) { Dir.glob(File.join(importer_unzip_path, '*.csv')) - [combined_csv_path] }
+    let(:original_csv_data) do
+      original_csv_paths.map { |csv_path| CSV.read(csv_path, headers: true).map(&:to_h) }.flatten
+    end
+    let(:original_headers) { original_csv_data.map(&:keys).flatten.uniq }
+    let(:combined_headers) { combined_csv_data.first.keys }
+    let(:added_headers) { %w[file model] }
+    let(:removed_headers) { %w[bs_isCommunity bs_isCollection] }
+
+    it 'has the correct headers' do
+      expect(combined_headers).to match_array(original_headers - removed_headers + added_headers)
+    end
+
+    # rubocop:disable RSpec/ExampleLength
+    it 'has all the data from the original CSVs into one' do
+      original_csv_data.each do |original_row|
+        original_row.except!(*removed_headers)
+
+        matching_row = combined_csv_data.find do |combined_row|
+          original_row.all? { |key, value| combined_row[key] == value }
+        end
+
+        expect(matching_row).not_to be_nil, "No matching row found for #{original_row}"
+      end
+    end
+    # rubocop:enable RSpec/ExampleLength
+
+    it 'has the collections at the top' do
+      expected_collection_count = combined_csv_data.count { |row| row['model'] == Hyrax.config.collection_model }
+      top_row_models = combined_csv_data.take(expected_collection_count).map { |row| row['model'] }
+      expect(top_row_models).to all(eq(Hyrax.config.collection_model))
+    end
+
+    after { csv_parser.send(:clean_up_upacked_files) }
+  end
+
   def clean_up
     FileUtils.rm_rf(importer_unzip_path) if Dir.exist?(importer_unzip_path)
   end
