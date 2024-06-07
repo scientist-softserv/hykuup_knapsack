f3967083f67db38c869e6bcdbf286352e93cc860
LaRita Robinson <larita@scientist.com>
2024-06-07 18:00:07 -0400

    Wrap config
    Build was throwing error:
    Hyrax::SimpleSchemaLoader::UndefinedSchemaError: No schema defined: mobius_work

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 # Use this to override any Hyrax configuration from the Knapsack
-
-Hyrax.config do |config|
-  config.register_curation_concern :mobius_work
+Rails.application.config.after_initialize do
+  Hyrax.config do |config|
+    config.register_curation_concern :mobius_work
+  end
 end
