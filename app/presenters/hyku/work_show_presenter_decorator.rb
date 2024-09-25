441d79a823daecfd9b9c09c3ba93b5409e97d7a4
April Rieger <april.rieger@yahoo.com>
2024-09-25 16:36:16 -0700

    Add better comment

--- app/presenters/hyku/work_show_presenter_decorator.rb
+++ app/presenters/hyku/work_show_presenter_decorator.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 # OVERRIDE Hyku to delegate Mobius properties
-# OVERRIDE Hyku to delegate Mobius property :date_published
+# OVERRIDE Hyku to delegate Unca property :date_published
 Hyku::WorkShowPresenter.delegate :rights, :relation, :coverage, :file_format, :date_published, to: :solr_document
