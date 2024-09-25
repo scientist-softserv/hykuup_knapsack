c8ab103c66241edc66eb0692c002619a2adffc8c
April Rieger <april.rieger@yahoo.com>
2024-09-25 11:03:49 -0700

    Updating off pairing with Kiah

--- app/presenters/hyku/work_show_presenter_decorator.rb
+++ app/presenters/hyku/work_show_presenter_decorator.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 # OVERRIDE Hyku to delegate Mobius properties
-
-Hyku::WorkShowPresenter.delegate :rights, :relation, :coverage, :file_format, to: :solr_document
+Hyku::WorkShowPresenter.class_eval do
+  delegate :date_published, to: :solr_document
+end
